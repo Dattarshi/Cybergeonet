325cac5980fda680e09682d89f71eb32a9745ec6
Dattarshi <dhp132000@gmail.com>
2025-03-22 16:54:36 -0400

    Fix static path issue for Render deployment

--- src/app.py
+++ src/app.py
@@ -3,11 +3,14 @@ from src.routes import main
 import os
 
 def create_app():
+    # Auto-detect project base directory
+    BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+
     app = Flask(
         __name__,
         static_url_path='/static',
-        static_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '../static')),
-        template_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates'))
+        static_folder=os.path.join(BASE_DIR, 'static'),
+        template_folder=os.path.join(os.path.dirname(__file__), 'templates')
     )
     app.register_blueprint(main)
     return app
