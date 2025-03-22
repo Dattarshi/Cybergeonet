8b679e5367561878deb0d792b7ce1ca8362fa894
Dattarshi <dhp132000@gmail.com>
2025-03-22 17:07:23 -0400

    Final restructure + static/template fix for Render

--- src/app.py
+++ src/app.py
@@ -1,16 +1,7 @@
 from flask import Flask
 from src.routes import main
-import os
 
 def create_app():
-    # Auto-detect project base directory
-    BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
-
-    app = Flask(
-        __name__,
-        static_url_path='/static',
-        static_folder=os.path.join(BASE_DIR, 'static'),
-        template_folder=os.path.join(os.path.dirname(__file__), 'templates')
-    )
+    app = Flask(__name__, template_folder="templates", static_folder="static")
     app.register_blueprint(main)
     return app
