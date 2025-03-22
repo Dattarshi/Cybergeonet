be9061dc5acc8adca0b28b593f15e497b51608a4
Dattarshi <dhp132000@gmail.com>
2025-03-22 17:48:18 -0400

    Add submit route & form

--- src/app.py
+++ src/app.py
@@ -3,5 +3,6 @@ from src.routes import main
 
 def create_app():
     app = Flask(__name__, template_folder="templates", static_folder="static")
+    app.secret_key = 'super-secret-key'  # Needed for flash messages
     app.register_blueprint(main)
     return app
